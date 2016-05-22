43562bebb16ac38da7ceeefba45dbb02bb2ad0c8
Hugo Mater <Hugo.Mater@hva.nl>
2016-05-22 22:46:03 +0200

    Tiles can now be clicked and adds 1 to the number

--- Sudoku/public_html/game/Tile.js
+++ Sudoku/public_html/game/Tile.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2016 hugo.
+ * Copyright 2016 Hugo Mater.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +16,32 @@
 
 function Tile(position, size)
 {
-    GameObject.call(this, position, 0, new Vector(1,1), null, false);
+    var collisionProperties = {};
+    collisionProperties.size = { x: size.x, y: size.y };
+    GameObject.call(this, position, 0, new Vector(1,1), null, false, new Collision(COLLISION_TYPE.RECTANGLE, this, collisionProperties));
+    
+    this.number = 0;
     
     Tile.prototype.Update = function(input, dt)
     {
-        
+        if(input.mouse.OnMouseClick(this, MOUSE_BUTTON.Left))
+        {
+            this.number++;
+            if(this.number > 9) this.number = 0;
+        }
     };
     
     Tile.prototype.Draw = function(context)
     {
+        //border strokerect
+        //strokeRect(position.x, position.y, size.x, size.y);
+        //strokeStyle "color";
         context.save();
         context.fillStyle = "white";
         context.fillRect(this.position.x - size.x / 2, this.position.y - size.y / 2, size.x, size.y);
+        context.font = "20px Arial";
+        context.fillStyle = "black";
+        context.fillText(this.number, this.position.x, this.position.y);
         context.restore();
     };
 };
